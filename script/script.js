88e60a0073fd28d054b81914644d2c814f0cbfa8
playerking3 <murilomarquesdecastro@gmail.com>
2023-04-14 19:54:44 -0300

    responsivo feito com animações

--- script/script.js
+++ script/script.js
@@ -3,9 +3,9 @@ var hamburguer = document.getElementById("burguer")
 hamburguer.addEventListener("click", function(){
     var nav2 = document.getElementById("header2")
 
-    if (nav2.className === "aberto"){
+    if (nav2.className === "aberto slide-down"){
         nav2.className = "header2"
     }else{
-        nav2.className = "aberto"
+        nav2.className = "aberto slide-down"
     }
 })
