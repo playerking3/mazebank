8b18059ae8e8f195b89098bc56ea089116988364
playerking3 <murilomarquesdecastro@gmail.com>
2023-05-01 20:37:16 -0300

    fiz a seção dos cartões de débito
    também coloquei um disclaimer que tudo isso é pra aprender

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// menu hamburguer
 var hamburguer = document.getElementById("burguer")
 
 hamburguer.addEventListener("click", function(){
@@ -9,3 +10,22 @@ hamburguer.addEventListener("click", function(){
         nav2.className = "aberto slide-down"
     }
 })
+
+//texto que some quando fecho a aba
+let disclaimer = document.getElementById("disclaimer")
+
+function some(){
+    disclaimer.style.display = "none"
+}
+
+// para mudar a cor do cartão de debito
+let foto = document.getElementById("mazeDebito").attributes
+
+function mudacor(x){
+    let link = x.attributes[0].value
+    foto.getNamedItem("src").value = "img/maze-debit-"+link+".png"
+    x.style.padding = "4px"
+}
+function bordanormal(x){
+    x.style.padding = "0px"
+}
